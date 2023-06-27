655b68c4ad1a5c9b170d1e08f9e4418db9535f8f
kaljulee <kaljuvlee@gmail.com>
2023-06-27 17:47:54 -0400

    comment out unmade pages

--- src/data/data.js
+++ src/data/data.js
@@ -7,29 +7,29 @@ export const tableOfContents = [
             {label: 'Fake Jiu Jitsu in One Image', slug: '/one-image'}
         ],
     },
-    {
-        title: {label: 'Theory'},
-        items: [{label: 'Dimensionality Reduction', slug: '/dimensionality-reduction'}]
-    },
-    {
-        title: {label: 'Fake Jiu Jitsu 2.0 LTS'},
-        items: [
-            {label: 'Overview', slug: '/lts-overview'},
-            {label: 'Turtle Space', slug: '/turtle-space'},
-            {label: 'Bridge and Shrimp', slug: '/bridge-and-shrimp'},
-            {label: 'Shotput', slug: '/shotput'},
-            {label: 'Connection', slug: '/connection'},
-            {label: 'Pattern', slug: '/pattern'},
-            {label: 'Timing', slug: '/timing'},
-        ]
-    },
-    {
-        title: {label: 'Appendix', slug: '/appendix'},
-        items: [
-            {label: 'Glossary', slug: '/glossary'},
-            {label: 'Supporting Materials', slug: '/supporting-materials'},
-        ]
-    },
+    // {
+    //     title: {label: 'Theory'},
+    //     items: [{label: 'Dimensionality Reduction', slug: '/dimensionality-reduction'}]
+    // },
+    // {
+    //     title: {label: 'Fake Jiu Jitsu 2.0 LTS'},
+    //     items: [
+    //         {label: 'Overview', slug: '/lts-overview'},
+    //         {label: 'Turtle Space', slug: '/turtle-space'},
+    //         {label: 'Bridge and Shrimp', slug: '/bridge-and-shrimp'},
+    //         {label: 'Shotput', slug: '/shotput'},
+    //         {label: 'Connection', slug: '/connection'},
+    //         {label: 'Pattern', slug: '/pattern'},
+    //         {label: 'Timing', slug: '/timing'},
+    //     ]
+    // },
+    // {
+    //     title: {label: 'Appendix', slug: '/appendix'},
+    //     items: [
+    //         {label: 'Glossary', slug: '/glossary'},
+    //         {label: 'Supporting Materials', slug: '/supporting-materials'},
+    //     ]
+    // },
 ];
 
 function createNavData() {
