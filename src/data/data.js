19c5a96e390905190e8b6d02ef3124cc8c879ce6
--unset <--unset>
2023-06-28 13:40:57 -0400

    new table of contents

--- src/data/data.js
+++ src/data/data.js
@@ -5,31 +5,42 @@ export const tableOfContents = [
             {label: 'Hello World', slug: '/intro'},
             {label: 'Fake Jiu Jitsu in One Image', slug: '/one-image'},
             {label: 'Fake Jiu Jitsu in One Sentence', slug: '/one-sentence'},
+            {label: 'Quick-Start', slug: 'quick-start'}
         ],
     },
-    // {
-    //     title: {label: 'Theory'},
-    //     items: [{label: 'Dimensionality Reduction', slug: '/dimensionality-reduction'}]
-    // },
-    // {
-    //     title: {label: 'Fake Jiu Jitsu 2.0 LTS'},
-    //     items: [
-    //         {label: 'Overview', slug: '/lts-overview'},
-    //         {label: 'Turtle Space', slug: '/turtle-space'},
-    //         {label: 'Bridge and Shrimp', slug: '/bridge-and-shrimp'},
-    //         {label: 'Shotput', slug: '/shotput'},
-    //         {label: 'Connection', slug: '/connection'},
-    //         {label: 'Pattern', slug: '/pattern'},
-    //         {label: 'Timing', slug: '/timing'},
-    //     ]
-    // },
-    // {
-    //     title: {label: 'Appendix', slug: '/appendix'},
-    //     items: [
-    //         {label: 'Glossary', slug: '/glossary'},
-    //         {label: 'Supporting Materials', slug: '/supporting-materials'},
-    //     ]
-    // },
+    {
+        title: {label: 'Theory'},
+        items: [
+            {label: 'Ecological Dynamics', slug: '/ecological-dynamics'},
+            {label: 'Dimensionality Reduction', slug: '/dimensionality-reduction'},
+            {label: 'What is a Fundamental?', slug: 'what-is-a-fundamental'},
+        ]
+    },
+    {
+        title: {label: 'Fake Jiu Jitsu 2.0 LTS'},
+        items: [
+            {label: 'Overview', slug: '/lts-overview'},
+            {label: 'Turtle Space', slug: '/turtle-space'},
+            {label: 'Bridge and Shrimp', slug: '/bridge-and-shrimp'},
+            {label: 'Shotput', slug: '/shotput'},
+            {label: 'Connection', slug: '/connection'},
+            {label: 'Pattern and Timing', slug: '/pattern-and-timing'},
+        ]
+    },
+    {
+        title: {label: 'Deprecated FJJ Versions'},
+        items: [
+            {label: 'FJJ v1.0', slug: '/fjj1'},
+            {label: 'FJJ v0.1', slug: 'proto-fjj'},
+        ]
+    },
+    {
+        title: {label: 'Appendix', slug: '/appendix'},
+        items: [
+            {label: 'Glossary', slug: '/glossary'},
+            {label: 'Supporting Materials', slug: '/supporting-materials'},
+        ]
+    },
 ];
 
 function createNavData() {
