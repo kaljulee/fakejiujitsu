5e57e3e31e74c73dd80a2c56df0d904401b717d2
--unset <--unset>
2023-06-27 21:53:15 -0400

    image language change

--- src/routes/one-image/+page.svelte
+++ src/routes/one-image/+page.svelte
@@ -7,7 +7,7 @@
     <p>Many people have created excellent <a href="https://www.google.com/search?q=bjj+mind+maps">mind maps of Brazilian Jiu Jitsu</a>.  But even the best of these are either highly incomplete or highly complex (or both).</p>
     <p>I believe the reason BJJ is so hard to map is because it uses a sub-optimal set of fundamental reference points.</p>
     <p>Like a geocentric mapping of planetary movement, a <a style="text-decoration: none;" href="https://www.graciemag.com/in-this-video-from-the-50s-carlos-and-helio-gracie-demonstrate-the-efficiency-of-bjj/">Helio-centric</a> mapping of BJJ results in a usable but unnecessarily complex system.</p>
-    <p>Fake Jiu Jitsu proposes a different set of fundamentals, resulting in a much simpler mental map.</p>
+    <p>Fake Jiu Jitsu proposes a different set of fundamentals, resulting in a much simpler map.</p>
 </PageWrapper>
 
 <style>
