34da67ef8b7393a785b4ea3eb32de27e4e6085fd
--unset <--unset>
2023-06-27 21:18:10 -0400

    capitalize helio

--- src/routes/one-image/+page.svelte
+++ src/routes/one-image/+page.svelte
@@ -6,7 +6,7 @@
     <img src={geo_to_helio} alt="Comparing Geocentric planet paths to heliocentric"/>
     <p>Many people have created excellent <a href="https://www.google.com/search?q=bjj+mind+maps">mind maps of Brazilian Jiu Jitsu</a>.  But even the best of these are either highly incomplete or highly complex (or both).</p>
     <p>I believe the reason BJJ is so hard to map is because it uses a sub-optimal set of fundamental reference points.</p>
-    <p>Like a geocentric mapping of planetary movement, a helio-centric mapping of BJJ results in a usable but unnecessarily complex system.</p>
+    <p>Like a geocentric mapping of planetary movement, a Helio-centric mapping of BJJ results in a usable but unnecessarily complex system.</p>
     <p>Fake Jiu Jitsu proposes a different set of fundamentals, resulting in a much simpler mental map.</p>
 </PageWrapper>
 
