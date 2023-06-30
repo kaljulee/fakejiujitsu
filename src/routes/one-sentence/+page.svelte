cd89e6b2160e7388f4abbd6c11f49c7a8ec52f36
--unset <--unset>
2023-06-30 14:17:27 -0400

    markdown one-sentence

--- src/routes/one-sentence/+page.svelte
+++ src/routes/one-sentence/+page.svelte
@@ -1,46 +1,7 @@
 <script>
     import PageWrapper from "$lib/components/PageWrapper.svelte";
-</script>
-<PageWrapper>
-    <div>
-        <p></p>
-        <p>In one informal sentence, a Fake Jiu Jitsu is:</p>
-        <p class="definition">An approach to training jiu jitsu that trades the concepts of 'technique' and 'position' for a small set of 1st order principles, and the higher-order principles derived from them.</p>
-        <p>This definition is in contrast to Brazilian Jiu Jitsu which can be described as</p>
-        <p class="definition">A set of positions (top and bottom) and techniques (offense and defense) learned from a teacher, practiced by a student.</p>
-        <p>There is very little room to talk about BJJ without referring to technique and position; it would arguably be incoherent to do so.
-        <p>If you are performing BJJ, you are almost certainly attempting to execute a technique from a position.  Further, these techniques and positions are ususally handed down from a teacher, fully formed.</p>
-        <br/>
-        <p>I argue that the concepts of position and technique are not actually fundamental, but instead are just one way of slicing up grappling into words.</p>
-        <p><i>Fake Jiu Jitsus</i> are alternative ways to talk about grappling, which may or may not include these concepts.</p>
-        <p>This is a claim about how grappling works, and what is really going on:</p>
-        <ul>
-            <li>when we wrestle around in pajamas</li>
-            <li>when we talk about wrestling around in pajamas</li>
-        </ul>
-        <p><strong>FJJ2</strong> is a specific model developed by the Nice Train Lab.</p>
-        <p>(I have an undergraduate degree in philosophy, so I know how to use confusing terminology responsibly)</p>
-        <p>A single-sentence summary of FJJ2:</p>
-        <p class="definition">Using <strong>Turtle Space Control</strong> to maintain your ability to apply alternating <strong>Shotput</strong>-powered <strong>Bridge and Shrimp</strong> movements onto an opponent you are <strong>Connected</strong> to, <strong>Timed</strong> contrary to an established <strong>Pattern</strong>.</p>
-        The bold terms are the model’s 1st-order principles.
-        <p>The statements in FJJ2 are <i>not</i> meant to necessarily be accurate descriptions of the world.  The intention is to have <strong>a grappling model that makes testable predictions.</strong></p>
-        <p>Once I have my model and predictions, I can</p>
-        <ul>
-            <li>run experiments</li>
-            <li>derive 2nd and 3rd order principles</li>
-            <li>note apparent deficiencies in the model</li>
-            <li>eventually revise the model</li>
-        </ul>
-
-        <p>So, to recap my definitions:</p>
-        <ul>
-            <li>BJJ is a real martial art defined around positions and techniques</li>
-            <li>Fake jiu jitus are alternative ways of describing and training grappling</li>
-            <li>FJJ2 is a specific model and prediction set</li>
-        </ul>
-    </div>
-</PageWrapper>
-
+    import SvelteMarkdown from "svelte-markdown";
+    const source = `
 <style>
     .definition {
         font-size: 18px;
@@ -50,3 +11,22 @@
         background: cornsilk;
     }
 </style>
+In one informal sentence, Fake Jiu Jitsu is:
+
+<p class="definition">An approach to training jiu jitsu that trades the concepts of 'technique' and 'position' for a small set of 1st order principles, and the higher-order principles derived from them.</p>
+
+This definition is in contrast to Brazilian Jiu Jitsu, which can be described as
+
+<p class="definition">A set of positions (top and bottom) and techniques (offense and defense) learned from a teacher, practiced by a student.</p>
+
+There is very little room to talk about BJJ without referring to technique and position; they are fundamental to the language of BJJ.
+
+Defined through its 1st-order principles, FJJ is
+<p class="definition">Using <strong>Turtle Space Control</strong> to maintain your ability to apply alternating <strong>Shotput</strong>-powered <strong>Bridge and Shrimp</strong> movements onto an opponent you are <strong>Connected</strong> to, <strong>Timed</strong> contrary to an established <strong>Pattern.</strong></p>
+
+Throughout this document, I'll be referring to Fake Jiu Jitsu as *FJJ* or *FJJ2*.  FJJ2 is the latest version of FJJ, but in general the terms can be used interchangeably.
+    `;
+</script>
+<PageWrapper>
+    <SvelteMarkdown source={source}/>
+</PageWrapper>
