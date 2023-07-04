b713daf9cb8eda357f6314a5d7b780e3fe44af4e
--unset <--unset>
2023-07-04 09:45:03 -0400

    1000 words

--- src/routes/one-thousand-words/+page.svelte
+++ src/routes/one-thousand-words/+page.svelte
@@ -4,4 +4,89 @@
 </script>
 
 <PageWrapper><SvelteMarkdown source={`
-Nothing here yet`}/></PageWrapper>
+<style>
+hr {
+    border: none;
+    border-top: 3px double #333;
+    color: #333;
+    overflow: visible;
+    text-align: center;
+    height: 5px;
+    margin: 30px 30px;
+    padding: 0 10px;
+}
+
+/*hr:after {*/
+/*    background: #fff;*/
+/*    content: '§§§';*/
+/*    padding: 0 4px;*/
+/*    position: relative;*/
+/*    top: -13px;*/
+/*}*/
+</style>
+# Techniques: Shadows on the Wall of Kano’s Cave
+
+Technique (I’m going to refer the entire library of BJJ techniques and positions as Technique) gets treated as an almost mystical fetish, like a physical spell that must be cast just right for the smaller person to defeat the larger.  When we encourage grapplers to focus on Technique, we are almost always encouraging them to ignore what actually matters - the specifics of the situation they are in.
+
+Grappling is a continuous flow of movement and intentions; in order to be able to talk about it and teach it we break it up into a language.  If we didn’t do this, we would not be able to communicate or teach.  We need a set of discrete ideas and concepts with which to talk about jiu jitsu.
+
+
+The language of BJJ is Technique, its definitions are based on large, relatively obvious limb positions.
+
+
+ Back on the ground and legs around the opponent’s waist?  Closed Guard.
+
+Legs around the opponent’s head and arm?  Triangle.
+
+Kneeling with your opponent pulled across your shoulders, belly-down?  Kata-guruma.
+
+There are variations and more details involved, but in the vast majority of cases, you can look at a photo and accurately determine the Techniques involved.
+
+Black Belts learned to grapple in this language, and it is the one they use to describe their success.  When we go to the experts to learn, we naturally assume that they know what they are talking about, and that they know how best to talk about it in the first place.
+
+One of the great strengths of this BJJ language is that it is easy to teach to beginners.  Because the definitions are static limb positions, it isn’t difficult to demonstrate them.  A day-one beginner may not be able to use Mount effectively, but you can put him in a recognizable Mount position with only a couple minutes work.
+
+This ease-of-introduction is also strong evidence of a sub-optimal system.
+
+The fact that you can put a novice into a recognizable Mount position is proof that “Mount” includes garbage jiu jitsu.
+I have felt training partners give up a superior position in a scramble in order to solidify an inferior half-guard, because they believe that is where real BJJ happens.
+
+Mount, Side Control, Arm Bars, Closed Guard – all of it can be performed terribly.
+You might say that a garbage armbar is not a “true armbar”, like that Scotsman we’ve all heard about.  I understand that small nuances can make or break an arm(bar), but that is my point: we don’t use these small nuances to define an armbar, we use the large limb positions.  In fact, when applied under full-resistance, we find these nuances to be situation-dependent.
+
+This is a feature of every Technique in BJJ – everything can be done poorly, and has some “invisible jiu jitsu”, some situation-specific modification, or just one last detail that the Professor will show you in class today.
+
+I find the high number of unique specifics to be highly suspicious.  Every Technique has its own set of nuances (which change depending on the current conditions).  This gives the BJJ language a high level of irreducible complexity.  I believe that these nuances are like the [epicycles](https://en.wikipedia.org/wiki/Deferent_and_epicycle) added to geocentric planetary motion.  I think there is something else, some other dimension that is orthogonal to the [Flatland](https://en.wikipedia.org/wiki/Flatland) of BJJ.  If we center our understanding of grappling on something other than Technique, I think these epicycles will disappear.
+
+ I think a careful, detailed study of Technique is like putting a finger under a microscope, trying to get a better view of [the moon it is pointing to](https://www.youtube.com/watch?v=CwPWDMvT21E).
+<hr/>
+Of course, I could be completely wrong about that.  In fact, we would expect a fake blue belt to be wrong and 100 years of BJJ to be right.
+
+But now that I see BJJ as Flatland, I can’t unsee it, and am dedicated to finding these hidden jiu-jitsu dimensions.
+
+In order to test this assertion (that the current BJJ language is just one of many possible languages, and a sub-optimal one at that), I will suppose that some [malicious Professor](https://en.wikipedia.org/wiki/Evil_demon) of the utmost power and cunning has employed all  his energies in order to deceive me.
+
+I must start with a clean slate, no techniques or positions imported from the authorities.
+
+Then carefully choose a small set of principles, all of which are assumed to change over time.
+
+Lastly, gather data about them by testing them in grappling experiments.
+
+But without an outside, authoritative source of Truth, I have the problem of verifying the results of my experiments.  I might import underhooks as a fundamental principle, then be so bad at jiu-jitsu that the experiments seem to say underhooks don’t matter.
+
+I can use high-level grappling as a reference point.  I am throwing out how experts talk about grappling, not the obvious fact of their grappling ability.
+
+So my first reference point: a fully mature FJJ should look very similar to BJJ.
+
+I think of this like the early heliocentric models of the solar system; ultimately they had to predict the same planetary positions that geocentric astronomists had observed.
+
+There was a period when there was very good reason to suspect that the Earth actually went around the sun, but the geocentric model was so well-developed that if you wanted to know where Mars was going to be tomorrow night, the geocentrists has a much more accurate answer.
+
+Improvements in telescope technology enabled more nuanced models that eventually let to a practically useful heliocentric astronomy.
+
+My second reference point: I should consider the quality of my data to be heavily impacted by my physical grappling literacy.  As I conduct experiments, my ability to feel what is going on will become better calibrated, and I can come to more confident conclusions.
+
+Once I have determined a more optimal grappling language and have a well-calibrated athletic sensitivity, then I should be able to create fake jiu-jitsu on the fly, without ever properly drilling Technique again.
+
+The mat is a night sky, I’m a telescope, and most people are looking at fingers.
+`}/></PageWrapper>
