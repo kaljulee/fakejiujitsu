f57ffbc556037b57db295a840f9c1a83016c088b
--unset <--unset>
2023-07-04 09:45:58 -0400

    deploy

---
+++ public/_app/immutable/nodes/10.d91a4c46.js
@@ -0,0 +1,7 @@
+import{S as f,i as l,s as $,y as a,z as i,A as u,g as m,d as p,B as c,F as g}from"../chunks/index.30d36c53.js";import{P as d}from"../chunks/PageWrapper.1f9b0c22.js";import{S as h}from"../chunks/SvelteMarkdown.2146b79f.js";function _(r){let t,n;return t=new h({props:{source:`
+Fake Jiu Jitsu 2 is an semi-scientific training method for learning jiu jitsu.
+Instead of taking information from authoritative outside sources, it develops all information in-house, using models and 1st-person experience as the source of truth.
+
+Fake Jiu Jitsus generally consist of a set of fundamentals, more complex principles based on those fundamentals, and exceptions that are outside the model but so universally useful they can't be ignored.
+Fake Jiu Jitsu 2 is the current Long-Term-Support version of Fake Jiu Jitsu.
+`}}),{c(){a(t.$$.fragment)},l(e){i(t.$$.fragment,e)},m(e,o){u(t,e,o),n=!0},p:g,i(e){n||(m(t.$$.fragment,e),n=!0)},o(e){p(t.$$.fragment,e),n=!1},d(e){c(t,e)}}}function J(r){let t,n;return t=new d({props:{$$slots:{default:[_]},$$scope:{ctx:r}}}),{c(){a(t.$$.fragment)},l(e){i(t.$$.fragment,e)},m(e,o){u(t,e,o),n=!0},p(e,[o]){const s={};o&1&&(s.$$scope={dirty:o,ctx:e}),t.$set(s)},i(e){n||(m(t.$$.fragment,e),n=!0)},o(e){p(t.$$.fragment,e),n=!1},d(e){c(t,e)}}}class x extends f{constructor(t){super(),l(this,t,null,J,$,{})}}export{x as component};
---
+++ public/_app/immutable/nodes/16.ae33ba55.js
@@ -0,0 +1,7 @@
+import{S as $,i as f,s as l,y as s,z as p,A as m,g as i,d as c,B as u,F as g}from"../chunks/index.30d36c53.js";import{P as _}from"../chunks/PageWrapper.1f9b0c22.js";import{S as d}from"../chunks/SvelteMarkdown.2146b79f.js";function w(a){let t,r;return t=new d({props:{source:`# Fake Jiu Jitsu 0.1
+
+* Turtle
+* Major Bridge
+* Minor Bridge
+* Major Shrimp
+* Minor Shrimp`}}),{c(){s(t.$$.fragment)},l(e){p(t.$$.fragment,e)},m(e,n){m(t,e,n),r=!0},p:g,i(e){r||(i(t.$$.fragment,e),r=!0)},o(e){c(t.$$.fragment,e),r=!1},d(e){u(t,e)}}}function S(a){let t,r;return t=new _({props:{$$slots:{default:[w]},$$scope:{ctx:a}}}),{c(){s(t.$$.fragment)},l(e){p(t.$$.fragment,e)},m(e,n){m(t,e,n),r=!0},p(e,[n]){const o={};n&1&&(o.$$scope={dirty:n,ctx:e}),t.$set(o)},i(e){r||(i(t.$$.fragment,e),r=!0)},o(e){c(t.$$.fragment,e),r=!1},d(e){u(t,e)}}}class v extends ${constructor(t){super(),f(this,t,null,S,l,{})}}export{v as component};
