8674e534699078d122e1eaf70dd1e9c296e5afce
--unset <--unset>
2023-07-05 09:01:19 -0400

    1000 word fixes

--- src/routes/one-thousand-words/+page.svelte
+++ src/routes/one-thousand-words/+page.svelte
@@ -7,41 +7,44 @@
 # Techniques:
 # Shadows on the Wall of Kano’s Cave
 
-Technique (I’m going to refer the entire library of BJJ techniques and positions as Technique) gets treated as an almost mystical fetish, like a physical spell that must be cast just right for the smaller person to defeat the larger.  When we encourage grapplers to focus on Technique, we are almost always encouraging them to ignore what actually matters - the specifics of the situation they are in.
+BJJ techniques and positions get treated as almost mystical fetishes, like physical spells that must be cast just right for the smaller person to defeat the larger.  When we encourage grapplers to focus on technique, we are almost always encouraging them to ignore what actually matters - *the specifics of the situation they are in*.
 
-Grappling is a continuous flow of movement and intentions; in order to be able to talk about it at all, let alone teach it, we need to break it up into discrete parts.  These parts form a language by which we can communicate with each other.  The basic unit of the BJJ vernacular is the Technique.
+Grappling is a continuous flow of movement and intentions; it needs to be broken up into discrete parts in order to be discussed.  These parts form a language by which we can communicate with each other.
 
+The basic unit of the BJJ vernacular is the *Technique* (I’m going to group both techniques and positions under the term Technique).
 
 Technique definitions are based on large, relatively obvious limb positions.
 
-
- Back on the ground and legs around the opponent’s waist?  Closed Guard.
-
-Legs around the opponent’s head and arm?  Triangle.
-
-Kneeling with your opponent pulled across your shoulders, belly-down?  Kata-guruma.
+* Back on the ground and legs around the opponent’s waist?  Closed Guard.
+* Legs around the opponent’s head and arm?  Triangle.
+* Kneeling with your opponent pulled across your shoulders, belly-down?  Kata-guruma.
 
 There are variations and more details involved, but in the vast majority of cases, you can look at a photo and accurately determine the Techniques involved.
 
 Black Belts learned to grapple in this language, and it is the one they use to describe their success.  When we go to the experts to learn, we naturally assume that they know what they are talking about, and that they know how best to talk about it in the first place.
 
-One of the great strengths of this BJJ language is that it is easy to teach to beginners.  Because the definitions are static limb positions, it isn’t difficult to demonstrate them.  A day-one beginner may not be able to use Mount effectively, but you can put him in a recognizable Mount position with only a couple minutes work.
+Dividing a continuous system into a language will usually come with trade-offs.  For example, a language with 10 allows for more nuanced communication than a language with 2, but it is more work to transmit and decode these more nuanced messages.
 
-This ease-of-introduction is also strong evidence of a sub-optimal system.
+One of the great strengths of the BJJ vernacular is that it is easy to teach to beginners.  Because the definitions are static limb positions, they can be understood quickly.  A day-one beginner may not be able to use Mount effectively, but you can put him in a recognizable Mount position with only a couple minutes work.  If necessary, you could even physically position the beginner's limbs yourself, and Mount would occur.
+
+This ease-of-introduction is hints at what we are giving up by using this language.
 
 The fact that you can put a novice into a recognizable Mount position is proof that “Mount” includes garbage jiu jitsu.
-I have felt training partners give up a superior position in a scramble in order to solidify an inferior half-guard, because they believe that is where real BJJ happens.
+I have felt training partners give up superior control in a scramble in order to solidify an inferior "real" position, because they believe that is where it is possible to be skillful.
 
 Mount, Side Control, Arm Bars, Closed Guard – all of it can be performed terribly.
-You might say that a garbage armbar is not a “true armbar”, like that Scotsman we’ve all heard about.  I understand that small nuances can make or break an arm(bar), but that is my point: we don’t use these small nuances to define an armbar, we use the large limb positions.  In fact, when applied under full-resistance, we find these nuances to be situation-dependent.
+You might say that a garbage armbar is not a “true armbar”, like that Scotsman we’ve all heard about.  I understand that small nuances can make or break an arm(bar), but that is my point: we don’t use these small nuances to define an armbar, we use the large limb positions.  In fact, when applied under full-resistance, we find these nuances to be fluid, winking in and out of existence depending on the situation.
 
 This is a feature of every Technique in BJJ – everything can be done poorly, and has some “invisible jiu jitsu”, some situation-specific modification, or just one last detail that the Professor will show you in class today.
 
-I find the high number of unique specifics to be highly suspicious.  Every Technique has its own set of nuances (which change depending on the current conditions).  This gives the BJJ language a high level of irreducible complexity.  I believe that these nuances are like the [epicycles](https://en.wikipedia.org/wiki/Deferent_and_epicycle) added to geocentric planetary motion.  I think there is something else, some other dimension that is orthogonal to the [Flatland](https://en.wikipedia.org/wiki/Flatland) of BJJ.  If we center our understanding of grappling on something other than Technique, I think these epicycles will disappear.
+The high number of unique specifics is the tradeoff made in the BJJ vernacular.  Every Technique has its own set of nuances (which change depending on the current conditions) to make it *actually* work.  This gives the BJJ language a high level of irreducible complexity.  I believe that these nuances are like the [epicycles](https://en.wikipedia.org/wiki/Deferent_and_epicycle) added to geocentric planetary motion.
 
- I think a careful, detailed study of Technique is like putting a finger under a microscope, trying to get a better view of [the moon it is pointing to](https://www.youtube.com/watch?v=CwPWDMvT21E).
-<hr/>
-Of course, I could be completely wrong about that.  In fact, we would expect a fake blue belt to be wrong and 100 years of BJJ to be right.
+I think there is something else, some other dimension that is orthogonal to the [Flatland](https://en.wikipedia.org/wiki/Flatland) of BJJ.  If we centered our understanding of grappling on something other than Technique, I believe these epicycles will disappear.
+
+ The careful, detailed study of Technique is like putting a finger under a microscope, trying to get a better view of [the moon it is pointing to](https://www.youtube.com/watch?v=CwPWDMvT21E).
+<hr />
+
+Of course, I could be completely wrong about that.  In fact, we would *expect* a [fake blue belt to be wrong](https://en.wikipedia.org/wiki/Dunning-Kruger_effect) and 100 years of BJJ to be right.
 
 But now that I see BJJ as Flatland, I can’t unsee it, and am dedicated to finding these hidden jiu-jitsu dimensions.
 
@@ -57,15 +60,15 @@ But without an outside, authoritative source of Truth, I have the problem of ver
 
 I can use high-level grappling as a reference point.  I am throwing out how experts talk about grappling, not the obvious fact of their grappling ability.
 
-So my first reference point: a fully mature FJJ should look very similar to BJJ.
+So my first reference point: *a fully mature FJJ should look very similar to BJJ*.
 
-I think of this like the early heliocentric models of the solar system; ultimately they had to predict the same planetary positions that geocentric astronomists had observed.
+I think of this like the early heliocentric models of the solar system; ultimately they had to predict the same planetary positions that geocentric astronomists had observed throughout history.
 
-There was a period when there was very good reason to suspect that the Earth actually went around the sun, but the geocentric model was so well-developed that if you wanted to know where Mars was going to be tomorrow night, the geocentrists has a much more accurate answer.
+While astronomers have always been observing the same planetary patterns, the exact nature of those observations has changed over time.
 
-Improvements in telescope technology enabled more nuanced models that eventually let to a practically useful heliocentric astronomy.
+For a while there was very good reason to suspect that the Earth actually went around the sun, but the geocentric model was so well-developed that if you wanted to know where Mars was going to be tomorrow night, the geocentrists has a much more accurate answer.  Improvements in telescope technology enabled more nuanced observations, which allowed for better models and eventually practical utility.
 
-My second reference point: I should consider the quality of my data to be heavily impacted by my physical grappling literacy.  As I conduct experiments, my ability to feel what is going on will become better calibrated, and I can come to more confident conclusions.
+My second reference point: *I should consider the quality of my data to be heavily impacted by my physical grappling literacy*.  As I conduct experiments, my ability to feel what is going on will become better calibrated, and I can come to more confident conclusions.
 
 Once I have determined a more optimal grappling language and have a well-calibrated athletic sensitivity, then I should be able to create fake jiu-jitsu on the fly, without ever properly drilling Technique again.
 
