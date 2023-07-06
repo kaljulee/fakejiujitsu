0e6b0226956afa1372ec3928b825e4bc61a83b35
--unset <--unset>
2023-07-06 09:40:26 -0400

    404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,11 @@
 <script>
     import PageWrapper from "$lib/components/PageWrapper.svelte";
-    import {page} from '$app/stores';
     import gif404 from "$lib/images/fourohfour.gif"
 </script>
 
 <PageWrapper>
     <div class="four-oh-four-wrapper">
-        <img src={gif404} alt="404 image"/>
+        <img src={gif404} alt="404 slam"/>
     </div>
 </PageWrapper>
 
