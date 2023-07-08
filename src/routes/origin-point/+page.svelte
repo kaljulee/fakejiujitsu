19e7cf7fce07a73e0221d1a8a4c83e453bec2ca2
--unset <--unset>
2023-07-08 13:42:39 -0400

    origin point updates

--- src/routes/origin-point/+page.svelte
+++ src/routes/origin-point/+page.svelte
@@ -15,13 +15,18 @@ td {
 border-bottom: 1px dashed gray;
 }
 </style>
+
+## Concern 1
+# Origin Point
+
 ## Summary
 * Origin Point is based on [Priit Mihkelson's zero points](https://youtube.com/clip/Ugkx-E8gi1nf8pbZdNuTQaa3ZEzBVyFJ822A).
 * The Origin Point is used as a reference for mapping jiu jitsu.
 * If you have a path back to the Origin, you are probably safe.
 * If you're opponent doesn't have a path back to the Origin, he is probably not safe.
 
-## Theory
+## Navigation units
+
 ### The Basic BJJ Navigation Unit: the Technique
 The primary navigation unit of BJJ is the Technique (includes positions).  They are understood in reference to each other by differentiation (half-guard is not the same as full guard) and by transition (you can transition to an armbar from bottom closed guard, but not from top closed guard).
 
@@ -46,7 +51,16 @@ Techniques are generally not reducible or composable, so each must be practiced 
 FJJ has an infinite number of points, all defined by distance and direction from the Origin Point.
 If you have the ability to return to the Origin Point, you are safe.  If you do not, you are not safe.
 
-### Comparison of BJJ/FJJ navigation units
+Distance from the Origin Point is defined as distance from [Priit's Defensive BJJ](https://youtu.be/jC6uFGCCLRY).
+
+This can be summarized as
+* keep your inside space (hip to shoulders) clear of opponent controls
+* keep your spine aligned such that you can bear weight
+
+Smaller, relatively simple points should allow a higher resolution when talking about grappling positions and movements.
+
+
+### Nav Unit Comparison
 | feature | BJJ | FJJ |
 | :----- | :-----: | :-----: |
 | basic unit | Technique | point |
@@ -56,20 +70,35 @@ If you have the ability to return to the Origin Point, you are safe.  If you do 
 
 ## General Use
 
-### Grappling Throttle
+* Position relative to the Origin is both an offensive and defensive concern.
+* Movements near the Origin promote stability.
+* Keeping your path clear to the Origin promotes safety.
+* Reaching increases distance from the Origin.
+* Distance from the Origin promotes instability.
 
+**Because distance from the Origin is associated with higher risk and more complex problems, mapping is expected to start in the Turtle Trenches.**
 
+As your appetite for risk increases, increase your distance from the Origin.  As a situation begins to feel more risky, decrease yor distance from the Origin.
 
-## BJJ/FJJ Translator
+This can be used as a speed- or intensity-control mechanism.  If things are getting too fast or intense, moving closer to the Origin should help.
+
+Moving further away from the Origin means more reaching, posting, and framing.  This will create more complex grappling configurations, eventually the equivalent of a BJJ Top Game.
+
+Reaching (distance from Origin) is required in order to effectively block your partner's path back to the Origin.
+
+**FJJ predicts that you will be successful when you are able to push your lab partner further away from the Origin than you are.**
+
+## BJJ/FJJ Translation Hints
+
+### Turtles All the Way Down
+It is expected that an FJJ novice will "lose" all gym rolls.  A BJJ offensive game and a BJJ top game are considered advanced and complex expressions of FJJ, and are build directly on a strong foundation of bottom and defensive game.
 
 ### Submissions are Control Positions
+The hi-res nature of using points instead of Technique means no transitioning between "position" and "submission".  Because FJJ is blind to the distinction, "submissions" occur only when you are able to keep your partner so far from the Origin, it is trivial to threaten chokes or breaks.
 
 ### Defense and Offense Played In All Positions
+You must defend your zero point positioning and maneuver your opponent into being out of position.  This is true on bottom and on top, so defense *and* offense need to be played in *all* BJJ positions.
 
-
-You must defend your zero point positioning and maneuver your opponent into being out of position.  This is true on bottom and on top, so defense and offense need to be played in all positions.
-
-* Don’t let them in turtle space
 `
 </script>
 <PageWrapper>
