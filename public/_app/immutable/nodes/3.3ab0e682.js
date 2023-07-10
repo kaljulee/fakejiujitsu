0d06fd5fa928cfd2af641c6079bd35a31011920c
--unset <--unset>
2023-07-10 16:18:25 -0400

    justifications

---
+++ public/_app/immutable/nodes/3.3ab0e682.js
@@ -0,0 +1,14 @@
+import{S as l,i as u,s as g,y as i,z as s,A as m,g as p,d as c,B as d,F as f}from"../chunks/index.2c6af615.js";import{P as h}from"../chunks/PageWrapper.b9d93ea4.js";import{S as $}from"../chunks/SvelteMarkdown.3abca1a5.js";import{S as w}from"../chunks/data.6a080591.js";function _(a){let t,n;return t=new $({props:{source:a[0]}}),{c(){i(t.$$.fragment)},l(e){s(t.$$.fragment,e)},m(e,r){m(t,e,r),n=!0},p:f,i(e){n||(p(t.$$.fragment,e),n=!0)},o(e){c(t.$$.fragment,e),n=!1},d(e){d(t,e)}}}function b(a){let t,n;return t=new h({props:{$$slots:{default:[_]},$$scope:{ctx:a}}}),{c(){i(t.$$.fragment)},l(e){s(t.$$.fragment,e)},m(e,r){m(t,e,r),n=!0},p(e,[r]){const o={};r&2&&(o.$$scope={dirty:r,ctx:e}),t.$set(o)},i(e){n||(p(t.$$.fragment,e),n=!0)},o(e){c(t.$$.fragment,e),n=!1},d(e){d(t,e)}}}function y(a){return[`
+# Justification by Anecdote
+
+There was an IBJJF where I decided I *was* going to hit an uchi-mata..  I [dead drilled](https://youtu.be/imjmLWj5WCU) it after class.  I performed it cleanly during the belt test, the head instructor gave me his secret tips.
+
+During the competition I was able to take multiple shots at uchi-mata, and was countered every time.
+Improvements in my form did not translate to getting takedowns (it did contribute to being taken down).
+
+During the pandemic I started training at home with friends.  I stopped trying to do throws and started locking out into a [Shrimp or a Bridge](${w.BRIDGE_SHRIMP}) while trying to put weight on my partner (lab partners described it as kind of a zombie shuffle).  Over time this has developed into being able to hit small trips on my peers.
+
+3 years practicing uchi-mata led to getting countered and thrown.
+
+3 years of practicing leaning on people like an idiot led to hitting destabilizing trips.
+`]}class B extends l{constructor(t){super(),u(this,t,y,b,g,{})}}export{B as component};
